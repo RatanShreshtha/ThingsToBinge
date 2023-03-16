c1d33621133a50ac930c19cfe7036614561e9705
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2023-03-16 20:49:09 +0530

    Add metadata for the website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      title: 'ThingsToBinge',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Lets keep you entertained by suggesting movie and series to binge on from many genres at random.' }
+      ],
+    },
+  },
 })
