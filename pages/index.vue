036936d18dfc9139204f65700a385c31055e89b7
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2023-03-18 15:20:10 +0530

    Complete index page

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,57 @@
         <p class="subtitle">
           For suggestions just click on movie or series.
         </p>
+
+        <div class="columns is-variable is-8 is-centered">
+          <div class="column is-one-third">
+            <div class="card">
+              <div class="card-content">
+                <h2 class="title">
+                  Movie
+                </h2>
+              </div>
+              <div class="card-image">
+                <figure class="image is-square">
+                  <img src="../assets/film-reel.png" alt="Movie">
+                </figure>
+              </div>
+              <div class="card-content">
+                <NuxtLink class="button is-dark is-outlined" :to="{ name: 'genres-type', params: { type: 'movie' } }">
+                  <span class="icon-text">
+                    <span>Movie</span>
+                    <span class="icon">
+                      <i class="fa-regular fa-circle-right"></i>
+                    </span>
+                  </span>
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="card">
+              <div class="card-content">
+                <h2 class="title">
+                  TV
+                </h2>
+              </div>
+              <div class="card-image">
+                <figure class="image is-square">
+                  <img src="../assets/retro-tv.png" alt="TV">
+                </figure>
+              </div>
+              <div class="card-content">
+                <NuxtLink class="button is-dark is-outlined" :to="{ name: 'genres-type', params: { type: 'tv' } }">
+                  <span class="icon-text">
+                    <span>TV</span>
+                    <span class="icon">
+                      <i class="fa-regular fa-circle-right"></i>
+                    </span>
+                  </span>
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
