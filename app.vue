4408643d3b401d553c7cfe61bd03e43275582725
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2023-03-21 00:37:34 +0530

    Use gtag in app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup>
+const { gtag: gtagOpts } = useRuntimeConfig().public
+const gtag = useGtag()
+
+gtag('js', new Date());
+gtag('config', gtagOpts.id);
+</script>
+
 <template>
   <AppNav />
   <NuxtPage />
