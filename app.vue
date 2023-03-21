04e59dca559ac48a820470ef26c0b8bc135bde14
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2023-03-22 00:10:06 +0530

    Fix gtag

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
-<script setup>
-const { gtag: gtagOpts } = useRuntimeConfig().public
-const gtag = useGtag()
-
-gtag('js', new Date());
-gtag('config', gtagOpts.id);
-</script>
-
 <template>
   <AppNav />
   <NuxtPage />
