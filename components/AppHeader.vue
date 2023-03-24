4de331d85789ec63afc43978866a404996e89767
Ratan Kulshreshtha <ratan.shreshtha@gmail.com>
2023-03-24 19:47:31 +0530

    Add imags in app navigation

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -8,39 +8,41 @@ const toggleNavbar = () => {
 
 <template>
   <header class="hero-head">
+    <nav class="navbar is-spaced" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <NuxtLink class="navbar-item has-text-weight-bold" :to="{ name: 'index' }">
+          <span class="is-size-5 has-text-weight-bold"> ThingsToBinge </span>
+        </NuxtLink>
 
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="container">
-        <div class="navbar-brand">
-          <NuxtLink class="navbar-item has-text-weight-bold" :to="{ name: 'index' }">
-            <span class="is-size-5 has-text-weight-bold"> ThingsToBinge </span>
+        <a role="button" :class="{ 'navbar-burger': true, 'is-active': state.isExpanded }" aria-label="menu"
+          :aria-expanded="state.isExpanded" data-target="navbarBasic" @click="toggleNavbar()">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasic" :class="{ 'navbar-menu': true, 'is-active': state.isExpanded }">
+        <div class="navbar-end">
+          <NuxtLink class="navbar-item has-text-weight-semibold" active-class="is-active" :to="{ name: 'index' }">
+            <nuxt-img src="/images/Home.png" alt="TV" placeholder />
+            Home
           </NuxtLink>
+          <NuxtLink class="navbar-item has-text-weight-semibold" active-class="is-active"
+            :to="{ name: 'genres-type', params: { type: 'movie' } }">
+            <nuxt-img src="/images/Movie.png" alt="TV" placeholder />
 
-          <a role="button" :class="{ 'navbar-burger': true, 'is-active': state.isExpanded }" aria-label="menu"
-            :aria-expanded="state.isExpanded" data-target="navbarBasic" @click="toggleNavbar()">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-
-        <div id="navbarBasic" :class="{ 'navbar-menu': true, 'is-active': state.isExpanded }">
-          <div class="navbar-end">
-            <NuxtLink class="navbar-item has-text-weight-semibold" active-class="is-active" :to="{ name: 'index' }">
-              Home
-            </NuxtLink>
-            <NuxtLink class="navbar-item has-text-weight-semibold" active-class="is-active"
-              :to="{ name: 'genres-type', params: { type: 'movie' } }">
-              Movie
-            </NuxtLink>
-            <NuxtLink class="navbar-item has-text-weight-semibold" active-class="is-active"
-              :to="{ name: 'genres-type', params: { type: 'tv' } }">
-              TV
-            </NuxtLink>
-            <NuxtLink class="navbar-item has-text-weight-semibold" active-class="is-active" :to="{ name: 'about' }">
-              About
-            </NuxtLink>
-          </div>
+            Movie
+          </NuxtLink>
+          <NuxtLink class="navbar-item has-text-weight-semibold" active-class="is-active"
+            :to="{ name: 'genres-type', params: { type: 'tv' } }">
+            <nuxt-img src="/images/TV.png" alt="TV" placeholder />
+            TV
+          </NuxtLink>
+          <NuxtLink class="navbar-item has-text-weight-semibold" active-class="is-active" :to="{ name: 'about' }">
+            <nuxt-img src="/images/About.png" alt="TV" placeholder />
+            About
+          </NuxtLink>
         </div>
       </div>
     </nav>
